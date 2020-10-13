55834ce3f059974e0306b4e9001c8e17d01acbe3
tharvik <tharvik@users.noreply.github.com>
2020-10-13 19:55:50 +0200

    app.component: rm css

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -9,7 +9,6 @@ import { ConfigService } from "./config.service";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
   public readonly datasets: List<Promise<Table>>;
