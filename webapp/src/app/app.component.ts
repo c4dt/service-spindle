dbf0bdb69ce8e237b755cb1546e645a7592b1447
tharvik <tharvik@users.noreply.github.com>
2021-01-08 17:58:45 +0100

    add revealer
    Closes: #33

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -9,6 +9,7 @@ import { ConfigService } from "./config.service";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
   public readonly datasets: List<Promise<Table>>;
